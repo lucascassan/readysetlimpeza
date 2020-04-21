2d23fa4b588037cf2eee3114fe497edd6863597d
cassan <lucascassan@live.com>
2020-04-21 16:59:55 -0300

    inicial

---
+++ ReadySetLimpezas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReadySetLimpezas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
